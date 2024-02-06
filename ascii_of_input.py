50c2265ecab794d0aaf8081649ce39b5849d98c8
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-02-06 09:45:48 +0530

    ASCII of each letter in input

---
+++ ascii_of_input.py
@@ -0,0 +1,16 @@
+n = input("Enter the string : ")
+
+print("The ascii value of each letter in string")
+for i in n:
+    print(f"ascii of {i} : {ord(i)}")
+
+'''
+Enter the string : Joswin
+The ascii value of each letter in string
+ascii of J : 74
+ascii of o : 111
+ascii of s : 115
+ascii of w : 119
+ascii of i : 105
+ascii of n : 110
+'''
