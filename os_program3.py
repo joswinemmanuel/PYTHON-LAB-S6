9771ce18790f4759b2ffe4cb370dd0665f38a32f
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-04-30 09:27:49 +0530

    split and splitext

--- os_program3.py
+++ os_program3.py
@@ -5,4 +5,5 @@ filepath, filename = os.path.split(os.getcwd())
 print("File path is : ", filepath)
 print("File name is : ", filename)
 
-print(os.path.splitext("data.jpg"))
+print(os.path.join(filepath, filename))
+print(os.path.splitext("data.jpg")) 
